fe36ee9daec1cfe05d86e24ec10cb0cf94156dd0
Fazle Choudhury <fac3@njit.edu>
2018-07-17 20:29:58 -0400

    Created and executed answer test successfully.

--- tests/Unit/AnswerTest.php
+++ tests/Unit/AnswerTest.php
@@ -13,8 +13,24 @@ class AnswerTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testSave()
     {
-        $this->assertTrue(true);
+        //make user
+        $user = factory(\App\User::class)->make();
+        $user->save();
+        //make question
+        $question = factory(\App\Question::class)->make();
+        //associate profileTest with user
+        $question->user()->associate($user);
+        $question->save();
+
+        $answer = factory(\App\Answer::class)->make();
+        //associate answer with user
+        $answer->user()->associate($user);
+        //associate answer with question
+        $answer->question()->associate($question);
+        $answer->save();
+        $this->assertTrue($answer->save());
+
     }
 }
