a89e30603769f1a8eeeaa012e3d159cd7f3a521e
Fazle Choudhury <fac3@njit.edu>
2018-07-19 11:59:32 -0400

    Updated the QustionController, home, question and question form.

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,16 @@ Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')
 
 Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
 
+
+
+
 Route::get('/questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answers.show');
+Route::get('/questions/{question_id}/answers/{answer_id}/edit', 'AnswerController@edit')->name('answers.edit');
+Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name('answers.store');
+Route::patch('/questions/{question_id}/answer/{answer_id}', 'AnswerController@update')->name('answers.update');
+Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@destroy')->name('answers.destroy');
+
 
 Route::resources([
-    'question' => 'QuestionController',
+    'questions' => 'QuestionController',
 ]);
